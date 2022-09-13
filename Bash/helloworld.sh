f0739e62d50c97a1ba064a102c4282e2e14833e9
Nottommy11 <thomas.marxsen11@gmail.com>
2022-09-13 17:26:33 -0500

    Bash File Added

---
+++ Bash/helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Hello, world"
+
+sleep 3
+
+echo "Good night, world"
+
+sleep 10
+
+
+echo "Good morning, world"
+
+sleep 3
+
+echo "Good afternoon, world"
+
+sleep 3
+
+echo "Goodbye, cruel, cruel, world"
