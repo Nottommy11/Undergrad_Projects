f64e00e8a73a6eb475c98bcdc283db47aa29e846
Nottommy11 <thomas.marxsen11@gmail.com>
2022-10-25 22:11:40 -0500

    Hack The Box VPN Script

---
+++ Bash/HTB.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Starting In..."
+
+sleep 1
+
+echo "3"
+
+sleep 1
+
+echo "2"
+
+sleep 1
+
+echo "1"
+
+sleep 1
+
+echo ""
+
+echo "Happy Hacking!"
+
+echo ""
+
+sleep 1
+
+cd /home/tommy11/Desktop && sudo openvpn starting_point_TheBigOOP.ovpn
+
+sleep 1
+
+echo ""
+
+echo "I hope you meant to do that!"
