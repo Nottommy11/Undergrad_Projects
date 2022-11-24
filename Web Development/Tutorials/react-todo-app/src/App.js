8dc7f7459a909cd0dc4963e62a9010a19770d18a
Nottommy11 <thomas.marxsen11@gmail.com>
2022-11-24 06:17:16 -0600

    React Todo App Tutorial

---
+++ Web Development/Tutorials/react-todo-app/src/App.js
@@ -0,0 +1,14 @@
+// YouTube Tutorial
+// https://youtu.be/E1E08i2UJGI
+import TodoList from "./components/TodoList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
