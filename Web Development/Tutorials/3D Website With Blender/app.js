7ba05de55615cc466c179134e31d0e580bdf1db6
Nottommy11 <thomas.marxsen11@gmail.com>
2023-01-29 01:31:22 -0600

    Tabs & additional images

--- Web Development/Tutorials/3D Website With Blender/app.js
+++ Web Development/Tutorials/3D Website With Blender/app.js
@@ -4,7 +4,7 @@ canvas.height = window.innerHeight;
 
 const context = canvas.getContext("2d");
 //CHANGE THIS WHEN RENDERED
-const frameCount = 30;
+const frameCount = 35;
 
 const currentFrame = (index) =>
   `./blender-img/${
