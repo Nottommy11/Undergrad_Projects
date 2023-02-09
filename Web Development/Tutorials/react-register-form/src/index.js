89496522a3ff62565947aecdf4a723c23fdef9f8
Nottommy11 <thomas.marxsen11@gmail.com>
2023-02-09 04:29:31 -0600

    React Register form

---
+++ Web Development/Tutorials/react-register-form/src/index.js
@@ -0,0 +1,13 @@
+//YouTube Tutorial: https://youtu.be/brcHK3P6ChQ
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
